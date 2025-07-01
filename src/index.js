88ce66fec5d9d34e80d913db37a061c728ccd3a5
Luis Manuel Jimenez Vega <l.jimenez.v078@edu.utc.mx>
2025-07-01 09:45:29 -0600

    libreria cors y morgan agregados

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 const express = require ('express');
 const app = express();
+const cors= require('cors');
+const morgan = require('morgan');
+app.use(cors());
+app.use(morgan('dev'));
 app.use(express.json());
 
 // configuracion icial del servidor  en el puerto
@@ -18,14 +22,6 @@ app.get('/', (req, res) => {
     );
 })
 
-//Nuestro primer WS Get  // A ESTAS RUTAS SE CONOCEN COMO END
-app.get('/hola', (req, res) => {    
-    res.json(
-        {
-            "Title": "CAMBIOS"
-        }
-    );
-})
 
 // rutas  responde el servidor a esta  ruta
 app.get('/mensaje/mensaje1', (req, res) => {    
@@ -39,39 +35,41 @@ app.get('/mensaje/mensaje1', (req, res) => {
 
 
 
-// VARIABLES EN UN  API    
-
-//para enviar variables al servidor usamos el metodo post
-//
-
 app.post('/sumar', (req, res) => {
-    const { num1, num2 } = req.body;
+    const {num1, num2} = req.body;
 
     if (!num1 || !num2){
-        return res.status(400).send({ error: 'faltan datos' });
+        return res.status(400).send({error: 'Faltan numeros para sumar'});
     }
-        const resultado = num1 + num2;
 
-        res.send ({ resultado });
+const resultado = num1 + num2;
+
+res.send({resultado});
 });
-
 
 app.post('/edad', (req, res) => {
-    const { nacimiento, actual } = req.body;
+    const {nacimiento, actual} = req.body;
 
     if (!nacimiento || !actual){
-        return res.status(400).send({ error: 'faltan datos' });
+        return res.status(400).send({error: 'Faltan numeros para sumar'});
     }
-        const edad = actual - nacimiento;
 
-        res.send ({ edad });
+const edad = actual - nacimiento;
+
+res.send({edad});
 });
+
+
+
+//Variables en una API suma de dos números 
+//Para enviar información al servidor usamos el metodo post
+
 
 //Iniciando el servidor
 app.listen(app.get('port'),()=>{
     console.log("ESTE ES MI SERVIDOR Y ESTA EN EL htt://localhost:4000");
 console.log("mi primer servidor ");
-
+ 
 
 
 });
